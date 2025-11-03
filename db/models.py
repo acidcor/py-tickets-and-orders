dda21828f97ce3d8f163421cc7e0b22deb88277a
acidcor <deadfoxua1337@gmail.com>
2025-11-03 21:18:21 +0100

    flake

--- db/models.py
+++ db/models.py
@@ -63,7 +63,9 @@ class MovieSession(models.Model):
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
-        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="orders"
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="orders"
     )
 
     class Meta:
