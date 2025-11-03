6ef4e416c1328ecd054eb75b609a5f42dce4be94
acidcor <deadfoxua1337@gmail.com>
2025-11-03 21:17:34 +0100

    fix

--- services/user.py
+++ services/user.py
@@ -1,7 +1,5 @@
 from django.contrib.auth import get_user_model
 
-from db.models import User
-
 
 def create_user(
         username: str,
@@ -39,7 +37,7 @@ def update_user(
         first_name: str = None,
         last_name: str = None,
 ) -> None:
-    user = get_user_model().objects.get(id=user_id)
+    user = get_user(user_id)
 
     if username:
         user.username = username
